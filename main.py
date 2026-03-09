f0f9cee9aea443b2a7da264c9368cf82d5703840
BaiR-MK <manbair396@gmail.com>
2026-03-09 18:15:17 +0300

    Add colorama for colored output

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # main.py
 import config
 from utils import calculate_sum
+from colorama import init, Fore, Style
 
-print(f"Запуск {config.APP_NAME}")
+init()
+
+print(f"{Fore.GREEN}Запуск {config.APP_NAME}{Style.RESET_ALL}")
 result = calculate_sum(10, 5)
-print(f"10 + 5 = {result}")
+print(f"{Fore.CYAN}10 + 5 = {result}{Style.RESET_ALL}")

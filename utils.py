d967db390f09590557bcb57bc2eadfe5ca643ca1
BaiR-MK <manbair396@gmail.com>
2026-03-09 18:17:47 +0300

    Add message formatting with colors

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 # utils.py
+from colorama import Fore, Style  # Добавили импорт
+
 def calculate_sum(a, b):
     return a + b
 
 def calculate_difference(a, b):
     return a - b
+
+def format_message(msg):
+    return f"{Fore.GREEN}{msg}{Style.RESET_ALL}"  # Новая функция
